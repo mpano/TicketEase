0a6b064a922184a9bd295c4dba8a966609e96f9b
mpano <mpano.akim@gmail.com>
2023-03-09 19:18:43 +0200

    Initial commit

---
+++ TicketEase/login.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace TicketEase
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+    }
+}
